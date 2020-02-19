5ca994dd99bb5229d3efe3668089eaed215badc8
unknown <viola4lfe@gmail.com>
2020-02-19 14:53:06 -0500

    can findById, findSteps, working on adding a Scheme

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,29 @@
 const db = require("../data/db-config.js");
 
 module.exports = {
- find,
+  add,
+  find,
+  findById,
+  findSteps
 };
 
+function add(scheme) {
+  return db("schemes").insert(scheme, "id");
+}
+
 function find() {
- return db("schemes");
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+
+function findSteps(id) {
+  return db("steps as s")
+    .join("schemes", "s.scheme_id", "schemes.id")
+    .select("s.instructions", "schemes.scheme_name")
+    .where("scheme_id", id);
 }
